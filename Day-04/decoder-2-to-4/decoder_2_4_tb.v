851ff7b16ca230a46216ce1bb0112b28507f267b
arutselvan-16 <arutselvanbaskaran@gmail.com>
2026-08-19 21:40:07 +0530

    Create decoder_2_4_tb.v

---
+++ Day-04/decoder-2-to-4/decoder_2_4_tb.v
@@ -0,0 +1,34 @@
+module decoder_2_4_tb;
+
+reg A1,A0;
+
+wire [3:0] Y;
+
+decoder_2_4 dut(
+    .A1(A1),
+    .A0(A0),
+    .Y(Y)
+);
+
+initial begin
+
+    $monitor("time=%0t,A1=%b,A0=%b,Y=%b",
+             $time,A1,A0,Y);
+
+    A1=0; A0=0;
+    #10;
+
+    A1=0; A0=1;
+    #10;
+
+    A1=1; A0=0;
+    #10;
+
+    A1=1; A0=1;
+    #10;
+
+    $finish;
+
+end
+
+endmodule
