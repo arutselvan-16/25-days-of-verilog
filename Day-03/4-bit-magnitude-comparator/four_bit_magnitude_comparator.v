9e39399518411e663a31d0a25914796812195002
arutselvan-16 <arutselvanbaskaran@gmail.com>
2026-08-19 21:09:39 +0530

    Create four_bit_magnitude_comparator.v

---
+++ Day-03/4-bit-magnitude-comparator/four_bit_magnitude_comparator.v
@@ -0,0 +1,24 @@
+module four_bit_magnitude_comparator(
+    input [3:0] A,
+    input [3:0] B,
+    output reg greater,
+    output reg equal,
+    output reg less
+);
+
+always @(*) begin
+
+    greater = 0;
+    less = 0;
+    equal = 0;
+
+    if (A > B)
+        greater = 1;
+    else if (A < B)
+        less = 1;
+    else
+        equal = 1;
+
+end
+
+endmodule
